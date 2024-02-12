39ce6696b601391cca0d993d7b747788bb0d3823
shahin009 <shaheenmis170879@gmail.com>
2024-02-13 00:47:59 +0600

    first commit

---
+++ script/triangel-calculator.js
@@ -0,0 +1,24 @@
+// base calculate function.
+function calculatorTriangleArea() {
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    // console.log(base)
+    // height calculation function.
+
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    // console.log(height)
+
+    const area = 0.5 * base * height;
+    console.log('result ber kore felsi:', area)
+    
+    // display area.
+
+    const triangleAreaSpan = document.getElementById('triangle-area');
+    triangleAreaSpan.innerText = area;
+
+    
+
+}
